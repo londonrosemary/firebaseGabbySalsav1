829a47b743d65c1fc2fe84142be768d816c18190
koltvictor <kolts.email@gmail.com>
2022-05-14 15:07:35 -0700

    adding spacing to products page

--- src/components/Products.js
+++ src/components/Products.js
@@ -18,6 +18,8 @@ export default function Products({ handleAddToCart }) {
   return (
     <div>
       <Header />
+      <br />
+      <br />
       <Container>
         {products.map((product) => {
           return (
