7ef547b1539d34e6e38d6b17c8d643cba7d874f2
koltvictor <kolts.email@gmail.com>
2022-05-16 11:49:57 -0700

    changing browser icon and title

--- src/components/About.js
+++ src/components/About.js
@@ -11,21 +11,22 @@ export default function About() {
         <Container className="aboutContainer">
           {/* <h1 className="aboutHead">About Gabby</h1> */}
           <p className="aboutBody">
-            <strong>Introducing Gabby</strong> The brains and style of the
+            <strong>Introducing Gabby</strong> the brains and style of the
             operation. Born and raised in Las Vegas, Gabby grew up learning how
-            to cook Mexican meals by helping her mom in the kitchen. She
-            painstakingly learned the ins-and-outs of traditional Mexican
-            cooking and developed her own flair over the years. Salsa Verde has
-            always been a traditional part of Gabby's family's meals and over
-            the years, the recipe has been developed into what it is today!
-            Still one of the musts in Gabby's household, she is so thrilled to
-            welcome everyone to bring the recipe into their own family
-            gatherings. The Red Salsa is the newest addition to Gabby's salsa
-            must-haves. Savory and delicious, all of Gabby's Salsas are
+            to cook authentic Mexican meals by helping her mom in the kitchen.
+            She painstakingly learned the ins-and-outs of traditional Mexican
+            cooking and over the years has developed her own personal flair.
+            Salsa Verde has always been a traditional part of Gabby's family's
+            meals and Gabby took that traditional taste and updated it to what
+            it is today! Still one of the musts in Gabby's household, she is so
+            thrilled to welcome everyone to bring the recipe into their own
+            family gatherings. The Red Salsa is the newest addition to Gabby's
+            salsa must-haves. Savory and delicious, all of Gabby's Salsas are
             traditional, delectible, and delicious. Guaranteed to have the
             freshest ingredients in every batch with <strong>big</strong>{" "}
             flavor!
           </p>
+          {/* <img src="https://i.imgur.com/pNEq9l5.png" alt="Gabby's Salsa" /> */}
         </Container>
         {/* <img
           src="http://s3.wp.wsu.edu/uploads/sites/2055/2017/07/iStock-477239111-1024x683.jpg"
